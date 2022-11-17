29ecfba43c7fdcc53f3e4f592db00c962cef95d6
joaopedrobenassi <joaopedrobenassi@gmail.com>
2022-11-17 15:52:28 -0300

    updates js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 
 
 const myButton = document.querySelector('button');
+const firstName = document.getElementById('field_name')
+const lastName = document.getElementById('field_last_name')
+const emailAddress = document.getElementById('field_email')
+const pass = document.getElementById('field_password')
+
+console.log(firstName.value)
 
 function enviar() {
-    alert('Thanks! Hope you enjoy!');
+
+    if(firstName.value == '' || lastName.value == '' || emailAddress.value == ''|| pass.value == ''){
+
+        alert('Please fill in all fields!')
+
+    } else {
+
+        alert('Thanks! Hope you enjoy!')
+    }
+
 }
 
-myButton.addEventListener('click', enviar);
+myButton.addEventListener('click', enviar)
 
 
 
